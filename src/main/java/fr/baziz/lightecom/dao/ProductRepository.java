7f87822a4b728dc7b09bbd510870b8d8c74c0075
Baziz <amir@baziz.fr>
2020-10-08 14:20:22 +0200

    pom xml modif + add respo dao

--- src/main/java/fr/baziz/lightecom/dao/ProductRepository.java
+++ src/main/java/fr/baziz/lightecom/dao/ProductRepository.java
@@ -1,15 +1,20 @@
-package fr.baziz.lightecom.repository;
+package fr.baziz.lightecom.dao;
+
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import fr.baziz.lightecom.entities.Product;
 
-public interface ProductRepository extends  JpaRepository<Product, Long> {
-    
+@RepositoryRestResource
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
-
-
-
-    
+    @RestResource(path = "/selectedProducts")
+    public List<Product> findBySelectedProducts();
+/*
+    @RestResource(path = "/productsByKeyword")
+    public List<Product> findByNameContains(String mc);
+*/
 }
--- src/main/java/fr/baziz/lightecom/entities/Product.java
+++ src/main/java/fr/baziz/lightecom/entities/Product.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Data ;
 import lombok.NoArgsConstructor;
 
 
